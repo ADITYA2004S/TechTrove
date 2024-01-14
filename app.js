23da37034bdbcc660047ba0634be6610c7ddcabd
Aditya <adityasinghofficial06@gmail.com>
2024-01-14 23:54:21 +0530

    Added express server

---
+++ app.js
@@ -0,0 +1,14 @@
+import bodyParser from "body-parser";
+import express from "express";
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Server is active at 3000");
+});
